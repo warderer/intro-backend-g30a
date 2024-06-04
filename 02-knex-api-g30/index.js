d52f5be6e8883a57aeb7a8ed8b9554de5805b376
César Guerra <warderer@gmail.com>
2024-06-03 20:08:21 -0600

    ✨ Add Post /homes homeRoutes

--- 02-knex-api-g30/index.js
+++ 02-knex-api-g30/index.js
@@ -1,5 +1,7 @@
 // #1 Llamar a la biblioteca de express (importarla)
 const express = require('express')
+// Importar las rutas de mi vista Home
+const homeRoutes = require('./routes/homeRoutes')
 
 // #2 Crear y configurar una aplicación (instancia) de express
 const app = express()
@@ -7,6 +9,7 @@ app.use(express.json()) // Decirle a express que vamos a usar JSON por defecto.
 app.use(express.urlencoded({ extended: true })) // Esto sirve para que express pueda entender los datos que vienen de un formulario.
 
 // #3 Crear o definir las rutas de mi servidor. (Endpoints)
+app.use('/api/v1', homeRoutes)
 
 // #4 Levantar el servidor en un puerto, por ejemplo el 3000.
 app.listen(3000, () => {
---
+++ 02-knex-api-g30/routes/homeRoutes.js
@@ -0,0 +1,10 @@
+// 1. Importar express y el router de express
+const express = require('express')
+const router = express.Router()
+const homeController = require('../controllers/homeControllers')
+
+// 2. Crear las rutas con el router
+router.post('/homes', homeController.createHome)
+
+// 3. Exportar el router
+module.exports = router
